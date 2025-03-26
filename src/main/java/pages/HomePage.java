07a88371aae7a7dae670c3a1f77ef780528a08c4
Samitha Maddekanda <savithrimaddekanda@gmail.com>
2025-03-27 03:07:47 +1300

    Refine code to match with some OOP concepts

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,23 +2,34 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
 
     WebDriver driver;
 
-    final By contactLink = By.linkText("Contact");
-    final By shopLink = By.linkText("Shop");
+    private final By contactLink = By.linkText("Contact");
+    private final By shopLink = By.linkText("Shop");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
+    private void clickLink(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
     public void navigateToContactPage() {
-        driver.findElement(contactLink).click();
+        clickLink(contactLink);
     }
 
     public void navigateToShopPage() {
-        driver.findElement(shopLink).click();
+        clickLink(shopLink);
     }
 }
--- src/test/java/testCases/ContactPageSubmitValidationTest.java
+++ src/test/java/testCases/ContactPageSubmitValidationTest.java
@@ -2,22 +2,33 @@ package testCases;
 
 import base.BaseClass;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.ContactPage;
 import pages.HomePage;
 
 public class ContactPageSubmitValidationTest extends BaseClass {
 
-    @Test(invocationCount = 5)
-    public void TC002(){
+    @DataProvider(name = "validFormData")
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                {"Samitha", "samitha@example.com", "Hello Samitha!"},
+                {"Anupama", "anupama@example.com", "Hello Anupama!"},
+                {"Yashoda", "yashoda@example.com", "Hello Yashoda!"},
+                {"Dasun", "dasun@example.com", "Hello Dasun!"},
+                {"Nevan", "nevan@example.com", "Hello Nevan!"}
+        };
+    }
+
+    @Test(dataProvider = "validFormData")
+    public void TC002(String forename, String email, String message) {
         HomePage homePage = new HomePage(driver);
         homePage.navigateToContactPage();
 
         ContactPage contactPage = new ContactPage(driver);
-        contactPage.fillMandatoryFields("Samitha", "samitha@example.com", "Hello Samitha!");
+        contactPage.fillMandatoryFields(forename, email, message);
         contactPage.clickSubmit();
         Assert.assertFalse(contactPage.areErrorsDisplayed(), "Success Message should be displayed");
-
 
     }
 
